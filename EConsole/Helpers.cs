86b4510793b34ee72a86fbf8d8cce6d26a045e0f
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-08-21 21:25:57 +0200

    Import EConsole code

---
+++ EConsole/Helpers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EConsole
+{
+    public static class Helpers
+    {
+        public static bool _continue()
+        {
+            ConsoleKeyInfo key;
+
+            do
+            {
+                Console.WriteLine("Press Enter to run again, or Esc to exit.");
+                key = Console.ReadKey();
+            } while (key.Key != ConsoleKey.Escape && key.Key != ConsoleKey.Enter);
+
+            return key.Key == ConsoleKey.Enter;
+        }
+
+        public static bool _isValidTextFileAsync(string path, int scanLength = 4096)
+        {
+            using (var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                var bufferLength = (int)Math.Min(scanLength, stream.Length);
+                var buffer = new char[bufferLength];
+
+                var bytesRead = reader.ReadBlock(buffer, 0, bufferLength);
+                reader.Close();
+
+                if (Math.Abs(bufferLength-bytesRead) > 3)
+                {
+                    throw new IOException($"There was an error reading from the file {path}");
+                }
+
+                char[] allowedChars = {
+                    (char)9,    // Horizontal Tab
+                    (char)10,   // New Line 
+                    (char)11,   // vertical Tab
+                    (char)13    // Carriage Return
+                };
+
+                for (int i = 0; i < bytesRead; i++)
+                {
+                    var c = buffer[i];
+
+                    if (char.IsControl(c) && !allowedChars.Contains(c))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+    }
+}
--- EConsole/Program.cs
+++ EConsole/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace EConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //check if there are args
+            if (args.Length == 0)
+            {
+                Console.Write("Please provide the path of the E# file to run: ");
+                Array.Resize(ref args, 1);
+                args[0] = Console.ReadLine();
+            }
+
+            var path = args[0].Replace("\"", "");
+
+            _runInterpreter(path);
+        }
+
+        private static void _runInterpreter(string path)
+        {
+            // check if the file exists
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"No file found at:\n{path}");
+                Console.ReadLine();
+                return;
+            }
+
+            // check if the file is readable text
+            if (!Helpers._isValidTextFileAsync(path))
+            {
+                Console.WriteLine($"File {path}");
+                Console.WriteLine("is not recognized as a valid E# file.");
+                Console.ReadLine();
+                return;
+            }
+
+            var interpreter = new EInterpreter.Worker();
+
+            do
+            {
+                interpreter.Go(File.ReadAllLines(path), Path.GetFileName(path));
+            } while (Helpers._continue());
+        }
+    }
+}
