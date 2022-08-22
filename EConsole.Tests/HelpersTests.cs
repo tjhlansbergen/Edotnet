4c9e7292a5eb2930eff652e652e1adc1d36c9c5f
Tako Lansbergen <tako@uptrends.com>
2022-08-22 15:18:21 +0200

    Add EConsole.Tests and import tests

---
+++ EConsole.Tests/HelpersTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EConsole.Tests
+{
+    [TestClass]
+    public class HelpersTests
+    {
+        [TestMethod]
+        public void TestIsValidTextFile()
+        {
+            // arrange
+            var path = Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent?.Parent?.Parent?.Parent?.FullName, "TestScripts");
+
+            // assert
+            foreach (var file in Directory.GetFiles(path))
+            {
+                Assert.IsTrue(EConsole.Helpers._isValidTextFileAsync(file));
+            }
+        }
+    }
+}
