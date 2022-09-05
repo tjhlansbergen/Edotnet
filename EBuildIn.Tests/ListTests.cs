c496e4a8095f144ba41a0c1b0c8317dd30d2ba2f
Tako Lansbergen <takolansbergen@xs4all.nl>
2022-09-05 20:51:00 +0200

    Add ListTests

---
+++ EBuildIn.Tests/ListTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EBuildIn.Tests
+{
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            // arrange
+            var list = new Variable(Types.List, subTypes: new List<Types> { Types.Text }, null);
+            var stri = new Variable(Types.Text, "Hi");
+
+            // act
+            EBuildIn.List.Add(list, stri);
+            
+            // assert
+            Assert.AreEqual(1, ((List<object>)list.Value).Count());
+            Assert.AreEqual("Hi", ((List<object>)list.Value).First());
+        }
+    }
+}
+
+
+
