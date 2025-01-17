a3f1c683e2c78f78567dcf17b5fc7cdddd5c13eb
Tako Lansbergen <takolansbergen@xs4all.nl>
2025-01-17 21:55:38 +0100

    Suppress null ref warnings

--- EBuildIn/File.cs
+++ EBuildIn/File.cs
@@ -7,12 +7,12 @@ public static class File
 
     public static Variable Read(Variable path)
     {
-        return new Variable(Types.Text, System.IO.File.ReadAllText((string)path.Value));
+        return new Variable(Types.Text, System.IO.File.ReadAllText((string)path.Value!));
     }
 
     public static Variable ReadLines(Variable path)
     {
-        var lines = System.IO.File.ReadAllLines((string)path.Value);
+        var lines = System.IO.File.ReadAllLines((string)path.Value!);
         var list = lines.Select(l => new Variable(Types.Text, l)).ToList();
         return new Variable(Types.List, new string[] { Types.Text.ToString() }, list);
     }
